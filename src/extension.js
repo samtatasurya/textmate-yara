a816f0b8c225964eb5331bc54bfd4d11550b3824
Thomas Gardner <tpgardner19@gmail.com>
2016-09-22 12:32:24 -0500

    removed some TypeScript stuff and added a dumpError 'function'

--- src/extension.js
+++ src/extension.js
@@ -1,13 +1,19 @@
 "use strict";
 
+// taken from js-beautify extension for VSCode
+// --------------------------------------------
 const vscode = require("vscode");
+const dumpError = e => {
+	if (e) console.log('yara err:', e);
+};
+// --------------------------------------------
 
 // extension entry point - must be reachable by VSCode
-export function activate() {
+function activate() {
     console.log("inside yara.activate()");
 }
 
 // extension exit point - must be reachable by VSCode
-export function deactivate() {
+function deactivate() {
     console.log("inside yara.deactivate()");
 }
