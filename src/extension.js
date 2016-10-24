810b9147b5c57a79942e310c540e581b957e18b1
Thomas Gardner <tpgardner19@gmail.com>
2016-10-23 23:42:05 -0500

    Added CompileRule and TestRule commands

--- src/extension.js
+++ src/extension.js
@@ -5,11 +5,25 @@ const vscode = require("vscode");
 // extension entry point - must be reachable by VSCode
 function activate(context) {
     console.log("[*] Installing YARA extension's commands");
-    var yaraInstallPath = vscode.workspace.getConfiguration('yara').installPath;
-    console.log("[*] YARA is installed in "+ yaraInstallPath)
-    vscode.commands.registerCommand("yara.ShowMessage", function () {
-        vscode.window.showInformationMessage("Hello World!");
-    });
+    var config = vscode.workspace.getConfiguration('yara');
+    if (config.installPath != null) {
+/*
+        if (!String(config.installPath).endswith("/")) {
+            config.installPath.concat("/");
+        }
+*/
+        console.log("[*] YARA is installed in "+ config.installPath)
+        vscode.commands.registerCommand("yara.CompileRule", function () {
+            vscode.window.showInformationMessage("Compiling " + "${currentFile}" + " with " + String(config.installPath) + "yarac64.exe");
+        });
+        vscode.commands.registerCommand("yara.TestRule", function() {
+            vscode.window.showInformationMessage("Invoking " + String(config.installPath) + "yara64.exe against " + "${currentFile}");
+        });
+    }
+    else {
+        console.log("[*] No YARA install found!");
+        vscode.window.showErrorMessage("No YARA installation folder specified!");
+    }
 }
 exports.activate = activate;
 
