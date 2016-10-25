5718f736a05c54b062b36ddeb7d00b923ad0a260
Thomas Gardner <tpgardner19@gmail.com>
2016-10-24 21:33:41 -0500

    created non-anonymous callback funcs for commands"

--- src/extension.js
+++ src/extension.js
@@ -1,28 +1,31 @@
 "use strict";
 
+const cmd = require("child_process").spawn("cmd");
 const vscode = require("vscode");
+var config = vscode.workspace.getConfiguration("yara");
+
+function cmd_compile_rule () {
+    var filePath = config.installPath + "\\yarac64.exe";
+    console.log("Compiling " + "${currentFile}" + " with " + filePath);
+    // yarac64.exe ${RULES_FILE} ${OUTPUT_FILE}
+}
+
+function cmd_test_rule () {
+    var filePath = config.installPath + "\\yara64.exe";
+    console.log("Invoking " + filePath + " against " + "${currentFile}");
+    // yara64.exe --timeout=${timeout} ${RULES_FILE} ${testFile}
+}
 
 // extension entry point - must be reachable by VSCode
 function activate(context) {
     console.log("[*] Installing YARA extension's commands");
-    var config = vscode.workspace.getConfiguration('yara');
     if (config.installPath != null) {
-/*
-        if (!String(config.installPath).endswith("/")) {
-            config.installPath.concat("/");
-        }
-*/
-        console.log("[*] YARA is installed in "+ config.installPath)
-        vscode.commands.registerCommand("yara.CompileRule", function () {
-            vscode.window.showInformationMessage("Compiling " + "${currentFile}" + " with " + String(config.installPath) + "yarac64.exe");
-        });
-        vscode.commands.registerCommand("yara.TestRule", function() {
-            vscode.window.showInformationMessage("Invoking " + String(config.installPath) + "yara64.exe against " + "${currentFile}");
-        });
+        vscode.commands.registerCommand("yara.CompileRule", cmd_compile_rule);
+        vscode.commands.registerCommand("yara.TestRule", cmd_test_rule);
     }
     else {
         console.log("[*] No YARA install found!");
-        vscode.window.showErrorMessage("No YARA installation folder specified!");
+        var disposable = vscode.window.showErrorMessage("No YARA installation folder specified!");
     }
 }
 exports.activate = activate;
