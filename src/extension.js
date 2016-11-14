d46a395b2957acd0dedbc75c59c21a2d75e198ae
Thomas Gardner <tpgardner19@gmail.com>
2016-11-13 19:47:08 -0600

    Fixed up cmd_compileRule fixed currentFile assignment, and added display_streams callback function

--- src/extension.js
+++ src/extension.js
@@ -4,19 +4,32 @@ const exec = require("child_process").exec;
 const vscode = require("vscode");
 const config = vscode.workspace.getConfiguration("yara");
 
+function display_streams(error, stdout, stderr) {
+    if (error) {
+        console.error(error.message);
+        var disposable = vscode.window.showErrorMessage(error.message);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    return "No compilation errors"
+}
+
 function cmd_compileRule () {
+    var basePath = "C:\\Users\\Thomas\\Documents\\GitHub\\textmate-yara\\";
     var yaracPath = config.installPath + "\\yarac64.exe";
-    var currentFile = vscode.workspace.textDocuments[0].filename;
+    var currentFile = vscode.workspace.textDocuments[0].fileName;
+    var testFile = basePath + "examples\\test.yara";
+    var outputFile = basePath + "examples\\output.yara";
+    var cmd = yaracPath + " " + currentFile + " " + outputFile;
     console.log("Compiling " + currentFile + " with " + yaracPath);
-    var args = ["C:\\Users\\Thomas\\Documents\\GitHub\\textmate-yara\\examples\\test.yara"];
-    exec(yaracPath, args);
+    exec(cmd, display_streams);
 }
 
 function cmd_testRule () {
     var yaraPath = config.installPath + "\\yara64.exe";
-    var currentFile = vscode.workspace.textDocuments[0].filename;
-    console.log("Invoking " + yaraPath + " against " + currentFile);
-    var args = ["--timeout", config.timeout, config.testFile];
+    var currentFile = vscode.workspace.textDocuments[0].fileName;
+//    console.log("Invoking " + yaraPath + " against " + currentFile);
     exec(yaraPath, args);
 }
 
