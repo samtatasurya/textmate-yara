8bb6a53f20465c6fff15327ccf8d5e75deee451a
Thomas Gardner <tpgardner19@gmail.com>
2016-11-27 20:08:20 -0600

    added an activate method

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,16 @@
 "use strict";
 
 import * as vscode from "vscode";
-import * as childproc from "child_process";
+// import * as childproc from "child_process";
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log("[*] Inside activate!");
+	var compileDisposable = vscode.commands.registerCommand('yara.CompileRule', () => {
+		vscode.window.showInformationMessage("Compile YARA Rule!");
+	});
+    var testDisposable = vscode.commands.registerCommand('yara.TestRule', () => {
+        vscode.window.showInformationMessage("Test YARA Rule!");
+    });
+	context.subscriptions.push(compileDisposable);
+    context.subscriptions.push(testDisposable);
+}
