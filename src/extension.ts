65736f28dd18fa76c46a9371b4eca6a5d306d888
Thomas Gardner <tpgardner19@gmail.com>
2016-11-27 20:57:27 -0600

    moved anon funcs to explicity funcs

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,21 @@
 
 import * as vscode from "vscode";
 
+// Compile our YARA rule(s) and display any errors, if they exist
+function cmdCompileRule() {
+	vscode.window.showInformationMessage("Compile YARA Rule!");
+}
+
+// Run our YARA rule(s) against a test file
+function cmdTestRule() {
+    vscode.window.showInformationMessage("Test YARA Rule!");
+}
+
 export function activate(context: vscode.ExtensionContext) {
-    console.log("[*] Inside activate!");
-	var compileDisposable = vscode.commands.registerCommand('yara.CompileRule', () => {
-		vscode.window.showInformationMessage("Compile YARA Rule!");
-	});
-    var testDisposable = vscode.commands.registerCommand('yara.TestRule', () => {
-        vscode.window.showInformationMessage("Test YARA Rule!");
-    });
+    // Registering our simple commands to call here
+	var compileDisposable = vscode.commands.registerCommand('yara.CompileRule', cmdCompileRule);
+    var testDisposable = vscode.commands.registerCommand('yara.TestRule', cmdTestRule);
+    // Letting VSCode know that we will want to use these commands
 	context.subscriptions.push(compileDisposable);
     context.subscriptions.push(testDisposable);
 }
