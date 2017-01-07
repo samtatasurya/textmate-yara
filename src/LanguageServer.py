2f8d7f2ce0a1cf713fe3d8d148f74a1346a7a67d
Thomas Gardner <tpgardner19@gmail.com>
2017-01-06 20:49:01 -0600

    Added parameter types to objects

--- src/LanguageServer.py
+++ src/LanguageServer.py
@@ -5,6 +5,9 @@ This implementation integrates with the 'yara-python' library to provide real-ti
 for compilation errors and warnings for YARA rules
 
 https://github.com/Microsoft/language-server-protocol/blob/master/protocol.md
+
+Bases many function/variable names on
+https://github.com/Microsoft/vscode-languageserver-node-example/blob/master/server/src/server.ts
 '''
 import logging
 import json
@@ -22,9 +25,9 @@ class Command(object):
         currently doesn't specify a set of well known commands
     So executing a command requires some tool extension code
     Input
-        title: Title of the command (e.g. save)
-        command: The identifier of the actual command handler
-        arguments: Arguments that the command handler should be invoked with
+        (string) title: Title of the command (e.g. save)
+        (string) command: The identifier of the actual command handler
+        (list) arguments: Arguments that the command handler should be invoked with
     '''
     def __init__(self, title, command, arguments=None):
         self.title = title
@@ -36,11 +39,12 @@ class Diagnostic(object):
     Represents a diagnostic, such as a compiler error or warning.
     Diagnostic objects are only valid in the scope of a resource.
     Input
-        msg_range: The range at which the message applies
-        message: The diagnostic's message
-        severity: The diagnostic's severity. If omitted, it is up to the client to interpret diagnostics as error, warning, info or hint
-        code: The diagnostic's code
-        source: A human-readable string describing the source of this diagnostic (e.g. 'typescript' or 'super lint')
+        (Range) msg_range: The range at which the message applies
+        (string) message: The diagnostic's message
+        (int) severity: The diagnostic's severity.
+            If omitted, it is up to the client to interpret diagnostics as error, warning, info or hint
+        (int|string) code: The diagnostic's code
+        (string) source: A human-readable string describing the source of this diagnostic (e.g. 'typescript' or 'super lint')
     '''
     def __init__(self, msg_range, message, severity=None, code=None, source=None):
         self.range = msg_range
@@ -62,8 +66,8 @@ class Location(object):
     '''
     Represents a location inside a resource, such as a line inside a text file
     Input
-        uri: Uniquely identifies the document. For documents store on disk this is a file URI
-        doc_range: Range in a text document; comparable to an editor selection
+        (string) uri: Uniquely identifies the document. For documents store on disk this is a file URI
+        (Range) doc_range: Range in a text document; comparable to an editor selection
     '''
     def __init__(self, uri, doc_range):
         self.uri = uri
@@ -74,15 +78,13 @@ class Position(object):
     Position in a text document expressed as zero-based line and character offset
     A position is between two characters like an 'insert' cursor in a editor
     Input
-        line: Line position in a document (zero-based)
-        character: Character offset on a line in a document (zero-based)
+        (int) line: Line position in a document (zero-based)
+        (int) character: Character offset on a line in a document (zero-based)
     '''
     def __init__(self, line, character):
         self.line = line
         self.character = character
 
-# Bases many function/variable names on
-# https://github.com/Microsoft/vscode-languageserver-node-example/blob/master/server/src/server.ts
 class Protocol(object):
     '''
     The Language Server protocol is used between a tool (the client) and
@@ -131,8 +133,8 @@ class Range(object):
     A range in a text document expressed as (zero-based) start and end positions
     A range is comparable to a selection in an editor. Therefore the end position is exclusive
     Input
-        start: Start position
-        end: End position
+        (Position) start: Start position
+        (Position) end: End position
     '''
     def __init__(self, start, end):
         self.start = start
