5be0037469b989e4130c95297f1f806c347ec917
Thomas Gardner <tpgardner19@gmail.com>
2017-02-20 06:58:05 -0600

    suppressed forEach errors in compileRule got installPath setting working moved yara/yarac into global Yara object

--- src/extension.ts
+++ src/extension.ts
@@ -7,13 +7,19 @@ class Yara {
     private config: vscode.WorkspaceConfiguration;
     private statusBarItem: vscode.StatusBarItem;
     private diagCollection: vscode.DiagnosticCollection;
+    private yarac: string;
+    private yara: string;
 
     // called on creation
     constructor() {
         this.config = vscode.workspace.getConfiguration("yara");
-        if (!this.config.has("installPath")) {
-            vscode.window.showErrorMessage("No YARA installation specified! Please set one in settings");
-            return;
+        if (this.config.has("installPath")) {
+            this.yarac = this.config.get("installPath") + "\\yarac";
+            this.yara = this.config.get("installPath") + "\\yara"
+        }
+        else {
+            this.yarac = "yarac";
+            this.yara = "yara";
         }
         this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
         this.diagCollection = vscode.languages.createDiagnosticCollection("yara");
@@ -21,10 +27,18 @@ class Yara {
 
     // Compile the current file
     public compileRule() {
-        const ofile: vscode.Uri = vscode.Uri.file("~\\AppData\\Local\\compiled.yarac");
-        let errors: Array<string> = [];
+        // need to initialize to null otherwise a compile error will happen in the else block
+        let ofile_path = null;
+        if (!this.config.has("compiled")) {
+            ofile_path = "~\\AppData\\Local\\yara_tmp.bin";
+            vscode.window.showWarningMessage(`No 'compiled' target is specified! Compiling to ${ofile_path}`);
+            return;
+        }
+        else {
+            ofile_path = this.config.get("compiled");
+        }
+        const ofile: vscode.Uri = vscode.Uri.file(ofile_path);
         let exit_code: number = 0;
-        let yarac: string = this.config.get("installPath") + "\\yarac.exe";
         let diagnostics: Array<vscode.Diagnostic> = [];
 
         const editor = vscode.window.activeTextEditor;
@@ -38,22 +52,28 @@ class Yara {
             return;
         };
         // run a sub-process and capture STDOUT to see what errors we have
-        const result = proc.spawn(yarac, [doc.fileName, ofile.toString()]);
+        const result = proc.spawn(this.yarac, [doc.fileName, ofile.toString()]);
         // regex to match line number in resulting YARAC output
         const pattern = RegExp("\\([0-9]+\\)");
         result.stderr.on('data', (data) => {
             data.toString().split("\n").forEach(line => {
-                let messages = line.trim().split(": ");
-                // dunno why this adds one to the result - for some reason the render is off by a line
-                let matches = pattern.exec(messages[0]);
-                let severity = messages[1] == "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning;
-                if (matches != null) {
-                    // remove the surrounding parentheses
-                    let line_no = parseInt(matches[0].replace("(", "").replace(")", "")) - 1;
-                    let start = new vscode.Position(line_no, doc.lineAt(line_no).firstNonWhitespaceCharacterIndex);
-                    let end = new vscode.Position(line_no, data.length);
-                    let line_range = new vscode.Range(start, end);
-                    diagnostics.push(new vscode.Diagnostic(line_range, messages.pop(), severity));
+                try {
+                    let parsed = line.trim().split(": ");
+                    // dunno why this adds one to the result - for some reason the render is off by a line
+                    let matches = pattern.exec(parsed[0]);
+                    let severity = parsed[1] == "error" ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning;
+                    if (matches != null) {
+                        // remove the surrounding parentheses
+                        let line_no = parseInt(matches[0].replace("(", "").replace(")", "")) - 1;
+                        let start = new vscode.Position(line_no, doc.lineAt(line_no).firstNonWhitespaceCharacterIndex);
+                        let end = new vscode.Position(line_no, data.length);
+                        let line_range = new vscode.Range(start, end);
+                        diagnostics.push(new vscode.Diagnostic(line_range, parsed.pop(), severity));
+                    }
+                }
+                catch (error) {
+                    vscode.window.showErrorMessage(error);
+                    console.log(`Typescript Error: ${error}`);
                 }
             });
         });
