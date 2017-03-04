eeafe87985fe428f9fdd9c88f8aa8deb54bbf0af
Thomas Gardner <tpgardner19@gmail.com>
2017-03-04 10:54:32 -0600

    Made Execute test more specific

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -12,19 +12,27 @@ suite("Yara Tests", () => {
     test("Compile Success", () => {
         let yara = new ext.Yara();
         vscode.workspace.openTextDocument(".\\rules\\compile_success.yara").then((document) => {
+            // the YARA rule should've compiled successfully
+            // errors/warnings don't get returned
             assert.equal(yara.compileRule(), 0);
         });
     });
     test("Compile Fail", () => {
         let yara = new ext.Yara();
         vscode.workspace.openTextDocument(".\\rules\\compile_fail.yara").then((document) => {
+            // the YARA rule should've failed
+            // errors/warnings get returned
             assert.notEqual(yara.compileRule(), 0);
         });
     });
     test("Execute", () => {
         let yara = new ext.Yara();
         vscode.workspace.openTextDocument(".\\rules\\test.yara").then((document) => {
-            assert.equal(yara.executeRule(), 0);
+            let results = yara.executeRule();
+            // ensure our test YARA rule matches our test file
+            // and that no errors or warnings were returned
+            assert.equal(results.matches, 1);
+            assert.equal(results.diagnostics, 0);
         });
     });
 });
