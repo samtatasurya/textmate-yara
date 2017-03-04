6092462369787add14083b60eb1f97c382c72ef0
Thomas Gardner <tpgardner19@gmail.com>
2017-03-04 11:11:46 -0600

    finally fixed test filepath issues

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,36 +5,52 @@
 //
 
 import * as assert from 'assert';
+import * as path from 'path';
 import * as vscode from 'vscode';
 import * as ext from '../src/yara';
 
 suite("Yara Tests", () => {
     test("Compile Success", (done) => {
         let yara = new ext.Yara();
-        vscode.workspace.openTextDocument(".\\rules\\compile_success.yara").then((document) => {
+        let filepath = path.join(__dirname, '..', '..', "test\\rules\\compile_success.yara");
+        vscode.workspace.openTextDocument(filepath).then((document) => {
             // the YARA rule should've compiled successfully
             // errors/warnings don't get returned
             assert.equal(yara.compileRule(), 0);
+            done();
+        },
+        (error) => {
+            console.log(error);
             done();
         });
     });
     test("Compile Fail", (done) => {
         let yara = new ext.Yara();
-        vscode.workspace.openTextDocument(".\\rules\\compile_fail.yara").then((document) => {
+        let filepath = path.join(__dirname, '..', '..', "test\\rules\\compile_fail.yara");
+        vscode.workspace.openTextDocument(filepath).then((document) => {
             // the YARA rule should've failed
             // errors/warnings get returned
             assert.notEqual(yara.compileRule(), 0);
+            done();
+        },
+            (error) => {
+            console.log(error);
             done();
         });
     });
     test("Execute", (done) => {
         let yara = new ext.Yara();
-        vscode.workspace.openTextDocument(".\\rules\\test.yara").then((document) => {
+        let filepath = path.join(__dirname, '..', '..', "test\\rules\\test.yara");
+        vscode.workspace.openTextDocument(filepath).then((document) => {
             let results = yara.executeRule();
             // ensure our test YARA rule matches our test file
             // and that no errors or warnings were returned
             assert.equal(results.matches, 1);
             assert.equal(results.diagnostics, 0);
+            done();
+        },
+            (error) => {
+            console.log(error);
             done();
         });
     });
