98df63386ee8c71f96376e63bd85756a2510c9a0
Thomas Gardner <tpgardner19@gmail.com>
2017-03-04 00:17:16 -0600

    removed comments

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,17 +1,13 @@
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
+// Based on https://github.com/Microsoft/vscode-wordcount/blob/master/test/extension.test.ts
 //
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as ext from '../src/yara';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Yara Tests", () => {
     test("Compile Success", () => {
         let yara = new ext.Yara();
