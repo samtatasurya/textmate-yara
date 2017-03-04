5c26ef211b05bebb3a1b7066eaadaf5a0b360c50
Thomas Gardner <tpgardner19@gmail.com>
2017-03-03 22:11:37 -0600

    added compileOnSave option

--- src/extension.ts
+++ src/extension.ts
@@ -7,15 +7,18 @@ import * as ext from "./yara";
 function activate(context: vscode.ExtensionContext) {
     console.log("Activating Yara extension")
     const YARA_MODE: vscode.DocumentFilter = { language: 'yara', scheme: 'file' };
-    let yara = new ext.Yara();
-    let saveSubscription = vscode.workspace.onDidSaveTextDocument(() => {yara.compileRule()})
-    let compileRule = vscode.commands.registerCommand("yara.CompileRule", () => {yara.compileRule()});
-    let execRule = vscode.commands.registerCommand("yara.ExecuteRule", () => {yara.executeRule()});
-    // Dispose of our objects later
-    context.subscriptions.push(yara);
-    context.subscriptions.push(saveSubscription);
-    context.subscriptions.push(compileRule);
-    context.subscriptions.push(execRule);
+    let config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("yara");
+        let yara = new ext.Yara();
+        if (config.get("compileOnSave")) {
+            let saveSubscription = vscode.workspace.onDidSaveTextDocument(() => {yara.compileRule()})
+            context.subscriptions.push(saveSubscription);
+        }
+        let compileRule = vscode.commands.registerCommand("yara.CompileRule", () => {yara.compileRule()});
+        let execRule = vscode.commands.registerCommand("yara.ExecuteRule", () => {yara.executeRule()});
+        // Dispose of our objects later
+        context.subscriptions.push(yara);
+        context.subscriptions.push(compileRule);
+        context.subscriptions.push(execRule);
 }
 
 function deactivate(context: vscode.ExtensionContext) {
