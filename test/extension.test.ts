597bbac6dd43a7b4e6b6ca216d04feb2ff38110d
Thomas Gardner <tpgardner19@gmail.com>
2017-03-05 19:24:58 -0600

    rearranged done() calls

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -13,30 +13,36 @@ suite("Yara Tests", () => {
     test("Compile Success", (done) => {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test\\rules\\compile_success.yara");
-        vscode.workspace.openTextDocument(filepath).then((document) => {
-            let results = yara.compileRule(document);
-            console.log(results);
-            // the YARA rule should've compiled successfully
-            // errors/warnings don't get returned
-            assert.equal(results, 0);
-        }, (error) => {
-            done(error);
-        });
-        done();
+        vscode.workspace.openTextDocument(filepath).then(
+            (document) => {
+                let results = yara.compileRule(document);
+                console.log(results);
+                // the YARA rule should've compiled successfully
+                // errors/warnings don't get returned
+                assert.equal(results, 0);
+                done();
+            }, (error) => {
+                // assert.fail(error);
+                done(error);
+            }
+        );
     });
     test("Compile Fail", (done) => {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test\\rules\\compile_fail.yara");
-        vscode.workspace.openTextDocument(filepath).then((document) => {
-            let results = yara.compileRule(document);
-            console.log(results);
-            // the YARA rule should've failed
-            // errors/warnings get returned
-            assert.equal(results, 1);
-        }, (error) => {
-            done(error);
-        });
-        done();
+        vscode.workspace.openTextDocument(filepath).then(
+            (document) => {
+                let results = yara.compileRule(document);
+                console.log(results);
+                // the YARA rule should've failed
+                // errors/warnings get returned
+                assert.equal(results, 1);
+                done();
+            }, (error) => {
+                // assert.fail(error);
+                done(error);
+            }
+        );
     });
     test("Execute", (done) => {
         let yara = new ext.Yara();
@@ -48,9 +54,10 @@ suite("Yara Tests", () => {
             // and that no errors or warnings were returned
             assert.equal(results.matches, 1);
             assert.equal(results.diagnostics, 0);
+            done();
         }, (error) => {
+            // assert.fail(error);
             done(error);
         });
-        done();
     });
 });
