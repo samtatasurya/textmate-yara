ad81c3731392b16c71a5cab209bd885ad914e156
Thomas Gardner <tpgardner19@gmail.com>
2017-04-25 21:44:26 -0500

    cleaned up unittests - they still don't work

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -13,75 +13,22 @@ suite("Yara Tests", () => {
     test("Compile Success", (done) => {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/compile_success.yara");
-        vscode.workspace.openTextDocument(filepath).then(
-            (document) => {
-                let diagnostics = yara.compileRule(document);
-                console.log(diagnostics);
-            //     let results = yara.compileRule(document);
-            //     let errors = 0;
-            //     // the YARA rule should've compiled successfully
-            //     // errors/warnings don't get returned
-            //     results.stderr.on("data", (data) => {
-            //         errors += data.toString().split("\n").length;
-            //         // "error: 3" shows up here. not sure why
-            //     });
-            //     results.on("close", (code, signal) => {
-            //         assert.equal(errors, 0);
-            //     });
-            //     done();
-            // }, (error) => {
-            //     assert.fail(error, "", `${error}`, "operator");
-            //     done(error);
-            }
-        );
+        vscode.workspace.openTextDocument(filepath).then((document) => {
+            yara.compileRule(document);
+        });
     });
     test("Compile Fail", (done) => {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/compile_fail.yara");
-        vscode.workspace.openTextDocument(filepath).then(
-            (document) => {
-                let diagnostics = yara.compileRule(document);
-                console.log(diagnostics);
-            //     let results = yara.compileRule(document);
-            //     let errors = 0;
-            //     // the YARA rule should've failed
-            //     // errors/warnings get returned
-            //     results.stderr.on("data", (data) => {
-            //         errors += data.toString().split("\n").length;
-            //     });
-            //     results.on("close", (code, signal) => {
-            //         assert.equal(errors, 3);
-            //     });
-            //     done();
-            // }, (error) => {
-            //     done(error);
-            }
-        );
+        vscode.workspace.openTextDocument(filepath).then((document) => {
+            yara.compileRule(document);
+        });
     });
     test("Execute", (done) => {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/test.yara");
-        vscode.workspace.openTextDocument(filepath).then(
-            (document) => {
-                let results = yara.executeRule(document);
-                let errors = 0;
-                let matches = 0;
-                // ensure our test YARA rule matches our test file
-                // and that no errors or warnings were returned
-                results.stdout.on("data", (data) => {
-                    matches += data.toString().split("\n").length;
-                });
-                results.stderr.on("data", (data) => {
-                    errors += data.toString().split("\n").length;
-                });
-                results.on("close", (code, signal) => {
-                    assert.equal(matches, 2);
-                    assert.equal(errors, 0);
-                });
-                done();
-            }, (error) => {
-                done(error);
-            }
-        );
+        vscode.workspace.openTextDocument(filepath).then((document) => {
+            yara.executeRule(document);
+        });
     });
 });
