19a2a96956abda424ed76da5bfa6221f7fbb24ac
Thomas Gardner <tpgardner19@gmail.com>
2017-04-26 07:07:47 -0500

    removed => funcs in favor of anon funcs

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -9,45 +9,39 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import * as ext from '../src/yara';
 
-suite("Yara Tests", () => {
-    test("Compile Success", (done) => {
+suite("Yara Tests", function() {
+    test("Compile Success", function(done) {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/compile_success.yara");
-        vscode.workspace.openTextDocument(filepath).then((document) => {
-            yara.compileRule(document).then(
-                function (result) {
-                    console.log('CompileSuccess promise complete: ' + JSON.stringify(result));
-                    done();
-                }
-            ).catch((err) => {
+        vscode.workspace.openTextDocument(filepath).then(function(document) {
+            yara.compileRule(document).then(function (result) {
+                console.log('CompileSuccess promise complete: ' + JSON.stringify(result));
+                done();
+            }).catch(function(err) {
                 console.log('CompileSuccess promise rejected: ' + JSON.stringify(err));
             });
         });
     });
-    test("Compile Fail", (done) => {
+    test("Compile Fail", function(done) {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/compile_fail.yara");
-        vscode.workspace.openTextDocument(filepath).then((document) => {
-            yara.compileRule(document).then(
-                function (result) {
-                    console.log('CompileFail promise complete: ' + JSON.stringify(result));
-                    done();
-                }
-            ).catch((err) => {
-                    console.log('CompileFail promise rejected: ' + JSON.stringify(err));
+        vscode.workspace.openTextDocument(filepath).then(function(document) {
+            yara.compileRule(document).then(function (result) {
+                console.log('CompileFail promise complete: ' + JSON.stringify(result));
+                done();
+            }).catch(function(err) {
+                console.log('CompileFail promise rejected: ' + JSON.stringify(err));
             });
         });
     });
-    test("Execute", (done) => {
+    test("Execute", function(done) {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/test.yara");
-        vscode.workspace.openTextDocument(filepath).then((document) => {
-            yara.executeRule(document).then(
-                function (result) {
-                    console.log('Execute promise complete: ' + JSON.stringify(result));
-                    done();
-                }
-            ).catch((err) => {
+        vscode.workspace.openTextDocument(filepath).then(function(document) {
+            yara.executeRule(document).then(function (result) {
+                console.log('Execute promise complete: ' + JSON.stringify(result));
+                done();
+            }).catch(function(err) {
                 console.log('Execute promise rejected: ' + JSON.stringify(err));
             });
         });
