df413cb0be2d77af0e0905b7a3407559f49e0fe7
Thomas Gardner <tpgardner19@gmail.com>
2017-04-28 19:27:23 -0500

    removed reject() method from Promise

--- src/yara.ts
+++ src/yara.ts
@@ -39,7 +39,7 @@ export class Yara {
             doc = editor.document;
         };
         // run a sub-process and capture STDOUT to see what errors we have
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             const result: proc.ChildProcess = proc.spawn(this.yarac, [doc.fileName, ofile.toString()]);
             result.stderr.on('data', (data) => {
                 data.toString().split("\n").forEach(line => {
@@ -103,7 +103,7 @@ export class Yara {
             doc = editor.document;
         };
         // run a sub-process and capture STDOUT to see what errors we have
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             let matches = 0;
             const result: proc.ChildProcess = proc.spawn(this.yara, [doc.fileName, tfile.fsPath]);
             const pattern: RegExp = RegExp("\\([0-9]+\\)");
