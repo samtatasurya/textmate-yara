d8d143e5511e91904f278dd9474244152d562373
Thomas Gardner <tpgardner19@gmail.com>
2017-05-08 06:21:25 -0500

    fixed indents

--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,19 @@ function activate(context: vscode.ExtensionContext) {
     console.log("Activating Yara extension");
     const YARA_MODE: vscode.DocumentFilter = { language: 'yara', scheme: 'file' };
     let config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("yara");
-        let yara = new ext.Yara();
-        let configWatcher = vscode.workspace.onDidChangeConfiguration(() => {yara.updateSettings()});
-        let compileRule = vscode.commands.registerTextEditorCommand("yara.CompileRule", () => {yara.compileRule(null)});
-        let execRule = vscode.commands.registerTextEditorCommand("yara.ExecuteRule", () => {yara.executeRule(null)});
-        if (config.get("compileOnSave")) {
-            let saveSubscription = vscode.workspace.onDidSaveTextDocument(() => {yara.compileRule(null)});
-            context.subscriptions.push(saveSubscription);
-        }
-        // Dispose of our objects later
-        context.subscriptions.push(configWatcher);
-        context.subscriptions.push(yara);
-        context.subscriptions.push(compileRule);
-        context.subscriptions.push(execRule);
+    let yara = new ext.Yara();
+    let configWatcher = vscode.workspace.onDidChangeConfiguration(() => {yara.updateSettings()});
+    let compileRule = vscode.commands.registerTextEditorCommand("yara.CompileRule", () => {yara.compileRule(null)});
+    let execRule = vscode.commands.registerTextEditorCommand("yara.ExecuteRule", () => {yara.executeRule(null)});
+    if (config.get("compileOnSave")) {
+        let saveSubscription = vscode.workspace.onDidSaveTextDocument(() => {yara.compileRule(null)});
+        context.subscriptions.push(saveSubscription);
+    }
+    // Dispose of our objects later
+    context.subscriptions.push(configWatcher);
+    context.subscriptions.push(yara);
+    context.subscriptions.push(compileRule);
+    context.subscriptions.push(execRule);
 }
 
 function deactivate(context: vscode.ExtensionContext) {
