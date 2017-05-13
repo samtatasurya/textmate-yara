27548fc1dd69e12029ddee0fc5eeb4e62debfb47
Thomas Gardner <tpgardner19@gmail.com>
2017-05-12 21:05:37 -0500

    experimenting with changing config settings

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -16,8 +16,6 @@ suite("YARA: Commands", function() {
         let yara = new ext.Yara();
         let filepath = path.join(__dirname, '..', '..', "test/rules/compile_success.yara");
         vscode.workspace.openTextDocument(filepath).then(function(document) {
-            let config = vscode.workspace.getConfiguration();
-            console.log(JSON.stringify(config.get("yara")));
             const promise = yara.compileRule(document);
             promise.then(function(diagnostics) {
                 let count:number = 0;
@@ -50,7 +48,17 @@ suite("YARA: Commands", function() {
 
     test("Compile Error", function(done) {
         let yara = new ext.Yara();
-        let filepath = path.join(__dirname, "..", "..")
+        let filepath = path.join(__dirname, "..", "..", "test/rules/compile_success.yara");
+        vscode.workspace.openTextDocument(filepath).then(function(done) {
+            let config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("yara");
+            let compiled_setting = config.get("compiled");
+            config.update("compiled", undefined, true).then(function(result) {
+                console.log(result);
+                console.log(config);
+            });
+            // done();
+            config.update("compiled", compiled_setting, true);
+        });
     });
 /*
     Ensure the appropriate number of diagnostics are returned when executing a collection of rules
@@ -73,43 +81,43 @@ suite("YARA: Commands", function() {
     });
 });
 
-suite("YARA: Settings", function() {
-/*
-    warning.yara file should return 1 diagnostic normally
-    returns 0 diagnostics when --no-warning flag is set
-    The next two tests also serve as a test on the $flags in general
-*/
-    test("No Warnings Enabled", function(done) {
-        let yara = new ext.Yara();
-        let filepath = path.join(__dirname, "..", "..", "test/rules/warning.yara");
-        // set the --no-warning flag and check if warnings are present
-        vscode.workspace.openTextDocument(filepath).then(function(document) {
-            const promise = yara.compileRule(document);
-            promise.then(function(diagnostics) {
-                let count:number = 0;
-                for (var i in diagnostics) {
-                    // Diagnostic Severity of 1 == Warning
-                    assert.equal(diagnostics[i].severity, 1);
-                }
-                done();
-            }).catch(function(err) {
-                console.log(err);
-            });
-        });
-    });
-/*
-    Let user know an option they have set doesn't exist
-    and that their compilation fails
-*/
-    test("Unknown Option Flag", function(done) {
-        let yara = new ext.Yara();
-        done();
-    });
-/*
-    Gracefully handle scenario where YARA isn't found at installPath
-*/
-    test("Install Path", function(done) {
-        let yara = new ext.Yara();
-        done();
-    });
-});
+// suite("YARA: Settings", function() {
+// /*
+//     warning.yara file should return 1 diagnostic normally
+//     returns 0 diagnostics when --no-warning flag is set
+//     The next two tests also serve as a test on the $flags in general
+// */
+//     test("No Warnings Enabled", function(done) {
+//         let yara = new ext.Yara();
+//         let filepath = path.join(__dirname, "..", "..", "test/rules/warning.yara");
+//         // set the --no-warning flag and check if warnings are present
+//         vscode.workspace.openTextDocument(filepath).then(function(document) {
+//             const promise = yara.compileRule(document);
+//             promise.then(function(diagnostics) {
+//                 let count:number = 0;
+//                 for (var i in diagnostics) {
+//                     // Diagnostic Severity of 1 == Warning
+//                     assert.equal(diagnostics[i].severity, 1);
+//                 }
+//                 done();
+//             }).catch(function(err) {
+//                 console.log(err);
+//             });
+//         });
+//     });
+// /*
+//     Let user know an option they have set doesn't exist
+//     and that their compilation fails
+// */
+//     test("Unknown Option Flag", function(done) {
+//         let yara = new ext.Yara();
+//         done();
+//     });
+// /*
+//     Gracefully handle scenario where YARA isn't found at installPath
+// */
+//     test("Install Path", function(done) {
+//         let yara = new ext.Yara();
+//         done();
+//     });
+// });
