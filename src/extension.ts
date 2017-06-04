8eefe99b856670fed065cd8fc41baddae58b48ce
Thomas Gardner <tpgardner19@gmail.com>
2017-06-04 08:43:01 -0500

    using anonymous functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,13 @@
 import * as vscode from "vscode";
 import * as ext from "./yara";
 
-
-function activate(context: vscode.ExtensionContext) {
+exports.activate = function(context: vscode.ExtensionContext) {
     console.log("Activating Yara extension");
     let yara = new ext.Yara();
     // Dispose of our objects later
     context.subscriptions.push(yara);
-}
-
-function deactivate(context: vscode.ExtensionContext) {
+};
+exports.deactivate = function(context: vscode.ExtensionContext) {
     console.log("Deactivating Yara extension");
-}
-
-exports.activate = activate;
-exports.deactivate = deactivate;
+    console.log(context.subscriptions.values());
+};
